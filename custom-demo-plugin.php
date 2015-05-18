319480ed1fff24b9cdf463d2a203317fbe3ebe92
Kirsten Cassidy <mantismamita@gmail.com>
2015-05-18 11:35:29 +0200

    Adds plugin header init

---
+++ custom-demo-plugin.php
@@ -0,0 +1,10 @@
+/*
+*	Plugin Name: Custom Demo Plugin
+*	Plugin URI: https://github.com/mantismamita/custom-demo-plugin
+*	Description: Adds functionality to demo site
+*	Version: 1.0
+*	Author: Kirsten Casidy
+*	Author URI: http://www.kirstencassidy.com
+*	License: GPL2
+*
+*/
